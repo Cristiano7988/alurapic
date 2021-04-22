297e8d97fe49050589cf00bffa2bd198df80e29f
cristiano7988 <cristianooliveiramorales@gmail.com>
2021-04-22 01:18:53 -0300

    Manipulando array

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
     <h1>{{ titulo }}</h1>
-    <img
-      :src="foto.url"
-      :alt="foto.title"
-      :title="foto.title"
-    />
+    <ul>
+      <li v-for="foto of fotos">
+        <img
+          :src="foto.url"
+          :alt="foto.title"
+          :title="foto.title"
+        />
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -14,10 +18,16 @@ export default {
   data() {
     return {
       titulo: "Alurapic",
-      foto: {
-        url: "https://www.petz.com.br/blog/wp-content/uploads/2021/03/piercing-para-cachorro-2.jpg",
-        title: "Cachorro"
-      }
+      fotos: [
+        {
+          url: "https://www.petz.com.br/blog/wp-content/uploads/2021/03/piercing-para-cachorro-2.jpg",
+          title: "Cachorro"
+        },
+        {
+          url: "https://super.abril.com.br/wp-content/uploads/2018/05/filhotes-de-cachorro-alcanc3a7am-o-c3a1pice-de-fofura-com-8-semanas1.png",
+          title: "Cachorro triste"
+        }
+      ]
     }
   }
 }
