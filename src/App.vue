a7b4d0abe077e7d5e4e52da5a161436cf551bb57
cristiano7988 <cristianooliveiramorales@gmail.com>
2021-04-22 18:51:16 -0300

    Menu de navegação entre as rotas

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,10 @@
 <template>
+    <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/cadastro">Cadastro</router-link></li>
+      </ul>
+    </nav>
     <div class="container">
       <router-view></router-view>
     </div>
