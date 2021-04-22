8812ec25d9af84f8fcb5631d080e9a068b356356
cristiano7988 <cristianooliveiramorales@gmail.com>
2021-04-22 18:40:36 -0300

    Criação das rotas para Home e Cadastro

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource' // modulo para requisições ajax
-
+import VueRouter from 'vue-router'
+import { routes } from './routes'
 Vue.use(VueResource) //registra o módulo importado
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
